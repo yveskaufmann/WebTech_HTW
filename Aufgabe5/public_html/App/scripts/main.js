33dc7008c2b435d3201387554f13cad28b472945
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-07-10 04:18:03 +0200

    Remove unused  public_html scripts

--- Aufgabe5/public_html/App/scripts/main.js
+++
@@ -1,8 +0,0 @@
-
-<!-- http://bootsnipp.com/snippets/featured/admin-nav-bar-with-popup-sign-in -->
-$(document).ready(function(){
-    //Handles menu drop down
-    $('.dropdown-menu').find('form').click(function (e) {
-        e.stopPropagation();
-    });
-});
